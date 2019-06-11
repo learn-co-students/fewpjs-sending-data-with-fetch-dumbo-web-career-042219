293b01bb2e02cd56cc78117bdd7a30047f8950c0
Wei <theweifeng@gmail.com>
2019-06-10 20:13:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+const userUrl = "http://localhost:3000/users";
+const body = document.querySelector("body");
+
+submitData = (userName, email) => {
+  return fetch(userUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: email
+    })
+  }).then(resp => resp.json())
+    .then(user => body.append(user.id))
+    .catch(error => body.append(error));
+  
+};
